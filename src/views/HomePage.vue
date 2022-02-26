302cd950dadca67c9cb0c47c58da51b771d55855
Cristian Torres <ctorres@codisa.com>
2022-02-26 17:38:21 -0600

    counterApp ionic+vue+pinia

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,68 +1,107 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+	<ion-page>
+		<ion-header :translucent="true">
+			<ion-toolbar>
+				<ion-title>Counter App</ion-title>
+			</ion-toolbar>
+		</ion-header>
+
+		<ion-content :fullscreen="true">
+			<ion-header collapse="condense">
+				<ion-toolbar>
+					<ion-title size="large">Counter App</ion-title>
+				</ion-toolbar>
+			</ion-header>
+
+			<div id="container">
+				<strong>{{ counterStore.count }}</strong>
+				<ion-row>
+					<ion-col>
+						<ion-button
+							color="tertiary"
+							shape="round"
+							@click="counterStore.decrement()"
+						>
+							-</ion-button
+						>
+						<ion-button
+							shape="round"
+							@click="counterStore.increment()"
+							>+</ion-button
+						>
+					</ion-col>
+				</ion-row>
+				<ion-button
+					color="danger"
+					@click="counterStore.$reset()"
+					>Reset</ion-button
+				>
+			</div>
+		</ion-content>
+	</ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+	IonRow,
+	IonCol,
+	IonButton,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { useCounterStore } from "@/stores/counter";
 
 export default defineComponent({
-  name: 'HomePage',
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  }
+	name: "HomePage",
+	components: {
+		IonContent,
+		IonHeader,
+		IonPage,
+		IonTitle,
+		IonToolbar,
+		IonRow,
+		IonCol,
+		IonButton,
+	},
+
+	setup() {
+		//const counterStore = useCounterStore();
+
+		return { counterStore: useCounterStore() };
+	},
 });
 </script>
 
 <style scoped>
 #container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+	text-align: center;
+
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 50%;
+	transform: translateY(-50%);
 }
 
 #container strong {
-  font-size: 20px;
-  line-height: 26px;
+	font-size: 20px;
+	line-height: 26px;
 }
 
 #container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
+	font-size: 16px;
+	line-height: 22px;
+
+	color: #8c8c8c;
+
+	margin: 0;
 }
 
 #container a {
-  text-decoration: none;
+	text-decoration: none;
 }
 </style>
